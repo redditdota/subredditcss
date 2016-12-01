238f0db2de37db9a3df14b1db092a2aa49cec306
Chris Arena <carena@infinio.com>
2016-12-01 15:18:50 -0500

    Added warning and size output to minifier.py

--- minifier.py
+++ minifier.py
@@ -1,8 +1,15 @@
 import requests
 
+reddit_css_max_length = 100 * 1024 # 100 KiB
+
 url = 'https://cssminifier.com/raw'
 data = {'input': open('stylesheet.css', 'rb').read()}
 response = requests.post(url, data=data)
 
 with open('stylesheet.css.mini', 'w') as m:
+    length = len(response.text)
+    if length > reddit_css_max_length:
+        print("WARNING: New stylesheet is too long.")
+    used = round(float(length)/reddit_css_max_length, 4)
+    print("Used size: %s%% of 100 KiB" % (used * 100))
     m.write(response.text)
